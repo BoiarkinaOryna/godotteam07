26a65904d6e2cfec13d1f02ef9885d87107aa883
BoiarkinaOryna <boyarkina.ar@gmail.com>
2024-03-17 22:06:07 +0200

    git phone buttons add

--- 1Lvl/door.gd
+++ 1Lvl/door.gd
@@ -1,16 +1,20 @@
 extends Area2D
 
 @export_file("*.tscn") var next_level
-
+var flag_portal_entered = false
+#
 func _on_body_entered(body):
-	if body.name == "Player":
-		get_tree().change_scene_to_file(next_level)
-# Called when the node enters the scene tree for the first time.
+	if body.name == "Player1":
+		flag_portal_entered = true
+		#
+		#
+## Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	pass
+	if flag_portal_entered == true:
+		get_tree().change_scene_to_file("res://Lvl2/Lvl2.tscn")
 
