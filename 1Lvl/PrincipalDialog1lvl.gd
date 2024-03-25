d8ad8ec3026e3a2b41d9d5bfe12d4b32d22ba9e2
Ivan <pataymolodoy@gmail.com>
2024-03-25 13:43:39 +0200

    fix 1lvl dialogic

--- 1Lvl/PrincipalDialog1lvl.gd
+++ 1Lvl/PrincipalDialog1lvl.gd
@@ -1,18 +1,29 @@
 extends Area2D
-var pause = false
+#var pause = false
+@onready var menu = $"../Player1/Moving"
+@onready var pause = $"../Player1/Pause"
+@onready var door = $"../Door"
+var flag = true
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+#endPrincipalDialogFirstLvl
+func _on_dialogic_signal(argument:String):
+	if argument == "endPrincipalDialogFirstLvl":
+		menu.visible = true
+		pause.visible = true
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 
 func _on_body_entered(body):
-	print(pause)
 	if body.name == "Player1":
-		pause = true
-		#set_process(not is_processing())
-		Dialogic.start("Principal_dialog")
+		if flag:
+			#pause = true
+			menu.visible = false
+			pause.visible = false
+			door.visible = true
+			#set_process(not is_processing())
+			Dialogic.start("Principal_dialog")
+			flag = false
