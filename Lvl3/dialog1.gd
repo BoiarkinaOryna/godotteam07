fa679daf3d833512ba7bb7be893edcca177ba1d8
BoiarkinaOryna <boyarkina.ar@gmail.com>
2024-03-26 21:30:28 +0200

    dialogic 3, 4

---
+++ Lvl3/dialog1.gd
@@ -0,0 +1,29 @@
+extends Area2D
+#var pause = false
+@onready var menu = $"../Player3/Moving"
+@onready var pause = $"../Player3/Pause"
+@onready var door = $"../Door"
+var flag = true
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	Dialogic.signal_event.connect(_on_dialogic_signal)
+#endPrincipalDialogFirstLvl
+func _on_dialogic_signal(argument:String):
+	if argument == "endPrincipalDialogFirstLvl":
+		menu.visible = true
+		pause.visible = true
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _on_body_entered(body):
+	if body.name == "Player3":
+		if flag:
+			#pause = true
+			menu.visible = false
+			pause.visible = false
+			door.visible = true
+			#set_process(not is_processing())
+			Dialogic.start("Principal_dialog")
+			flag = false
